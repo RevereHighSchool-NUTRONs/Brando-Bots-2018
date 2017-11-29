479c0e03e30c3512f57977ff740163b55f8d8327
Josh Young <josh@Joshs-MacBook-Pro.local>
2017-11-28 19:28:33 -0500

    auto code and drivetrain control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/BBGyro.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+/**
+ * Created by josh on 11/28/17.
+ */
+
+public class BBGyro {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsytems/Util.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Subsytems;
+
+/**
+ * Created by josh on 11/28/17.
+ */
+
+public class Util {
+}
